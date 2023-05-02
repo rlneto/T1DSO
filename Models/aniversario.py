d64f93f3a66c6768551dde3383511e22bf07f120
rlneto <rlneto@live.com>
2023-05-02 00:46:37 -0300

    Início da implementação do tipo de evento "Aniversario" (fixes/improvements)

--- Models/aniversario.py
+++ Models/aniversario.py
@@ -2,6 +2,30 @@ from Models.evento import Evento
 
 
 class Aniversario(Evento):
-    def __init__(self, data: str, titulo: str, aniversariante: str,
-                 descricao: str = "Sem descrição."):
+
+    def __init__(self, data: str, titulo: str, descricao: str = "Sem descrição."):
         super().__init__(data, titulo, descricao)
+
+    @property
+    def data(self):
+        return self.__data
+
+    @data.setter
+    def data(self, nova_data: str):
+        self.__data = nova_data
+
+    @property
+    def titulo(self):
+        return self.__titulo
+
+    @titulo.setter
+    def titulo(self, novo_titulo: str):
+        self.__titulo = novo_titulo
+
+    @property
+    def descricao(self):
+        return self.__descricao
+
+    @descricao.setter
+    def descricao(self, nova_descricao: str):
+        self.__descricao = nova_descricao
