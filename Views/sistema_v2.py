71bdfbd82045e66fef6cda86c853ab45bfac19b9
rlneto <rlneto@live.com>
2023-06-04 22:09:42 -0300

    sistema_v2

---
+++ Views/sistema_v2.py
@@ -0,0 +1,31 @@
+def bar():
+    print("=" * 20)
+
+
+class SistemaV:
+    def __init__(self):
+        pass
+
+    def mensagem(self, message: str):
+        print(message)
+
+    def capturar(self, message) -> int:
+        while True:
+            try:
+                escolha = int(input(message))
+            except ValueError:
+                self.mensagem("Erro: A opção escolhida deve ser um número"
+                              " inteiro.")
+            else:
+                break
+        return escolha
+
+    def menu(self) -> int:
+        bar()
+        print("      ONIVERSO      ")
+        bar()
+        opcoes = ("1 - Criar Calendário", "2 - Acessar Calendário",
+                  "9 - Visualizar Calendários (Admin)", "0 - Sair")
+        for opcao in opcoes:
+            print(opcao)
+        return self.capturar("Selecione uma opção: ")
